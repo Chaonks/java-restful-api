427eddf7445c83199e0d739017dfb893f55868b9
ricardo <chaonks@gmail.com>
2023-09-11 21:20:34 -0300

    Bugfix CORS

--- src/main/java/com/playlist/OrganizerApplication.java
+++ src/main/java/com/playlist/OrganizerApplication.java
@@ -1,8 +1,11 @@
 package com.playlist;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+import io.swagger.v3.oas.annotations.servers.Servers;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default ServerURL")})
 @SpringBootApplication
 public class OrganizerApplication {
 
